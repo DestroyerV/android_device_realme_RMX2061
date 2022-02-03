b7100e9f65e54486bb4711c3c7714f979d4d5265
DestroyerV <mishravaibhav12321@gmail.com>
2022-02-03 06:04:52 +0000

    PPUI BRING UP

--- aosp_RMX2061.mk
+++ aosp_RMX2061.mk
@@ -12,8 +12,8 @@ TARGET_BOOT_ANIMATION_RES := 1440
 
 WITH_CORE_GAPPS := true
 
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common stuff
+$(call inherit-product, vendor/aosp/common_full_phone.mk)
 
 # Device identifier
 PRODUCT_NAME := aosp_RMX2061
@@ -24,11 +24,16 @@ PRODUCT_MANUFACTURER := Realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2061" \
     TARGET_DEVICE="RMX2061"
+
+#PPUI stuff
+IS_PHONE := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
